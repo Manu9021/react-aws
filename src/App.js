692b93c9675816400d50116ea8a501adb4d564f7
Manou <sanrichouin@gmail.com>
2025-04-22 17:00:51 +0200

    change app

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="manou.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React manou
         </a>
       </header>
     </div>
